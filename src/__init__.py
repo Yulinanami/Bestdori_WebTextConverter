2a08f99663d7f701cef5ef929e45a1d0082415c6
Yulinanami <yiy27532@gmail.com>
2026-01-13 17:08:04 +0800

    clean

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,1 @@
-# 日志基础配置（让各模块的 logger 输出到标准输出）
-import logging
-import sys
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - [%(levelname)s] - %(name)s - %(message)s",
-    stream=sys.stdout,
-)
+# src 包初始化文件
